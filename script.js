a5f97683299f5401eaf32c54eb5668cfdbf30184
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2025-07-11 15:58:41 +0530

    Reworked the slide change functionality and added a setInterval function

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
 const slidecount = document.getElementsByClassName("imagedisplay").length;
 let currentslide = 1;
-let currentnav = 1;
-
 
 const displaySlides = (currentslide) => {
+    if ( currentslide === 5){
+        clearInterval(interval);
+    }
+
+
     const imageClass = document.getElementsByClassName("imagedisplay");
     let slideID = document.getElementById(String(currentslide));
 
-
-    console.log("imageClass is: ",imageClass);
-    //reset
     Array.from(imageClass).forEach((e)=>{
-        e.style.display = "none";
+        e.classList.remove("showimage");
+        e.classList.add("hide");
     })
 
-    //display the current image alone
     Array.from(imageClass).forEach((e)=>{
         if ( e === slideID){
-            e.style.display = "block";
+            e.classList.remove("hide");
+            e.classList.add("showimage");
         }
     })
 }
 
-
-const displayNav = (currentnav) =>{
-    let navID = document.getElementById("nav"+String(currentnav));
-    // console.log(navID, "is the navID");
-
-}
-
-displayNav(currentnav);
 const leftSliderEL = document.getElementById("leftslider").addEventListener("click",()=>{
     if ( currentslide === 1 ){
         return;
@@ -37,7 +30,10 @@ const leftSliderEL = document.getElementById("leftslider").addEventListener("cli
     else{
         currentslide = currentslide - 1;
         displaySlides(currentslide);
+        modifyNav(currentslide);
     }
+
+    clearInterval(interval);
 });
 
 const rightSliderEL = document.getElementById("rightslider").addEventListener("click",()=>{
@@ -47,8 +43,32 @@ const rightSliderEL = document.getElementById("rightslider").addEventListener("c
     else{
         currentslide = currentslide + 1;
         displaySlides(currentslide);
+        modifyNav(currentslide);
     }
+    clearInterval(interval);
 });
 
+const modifyNav = (navid) =>{
+    const navbuttons = document.getElementsByClassName("navbutton");
+    Array.from(navbuttons).forEach((e)=>{
+        e.classList.remove("disableColor");
+        e.classList.add("enableColor")
+    })
+
+    Array.from(navbuttons).forEach((e)=>{
+        if ( e.id === "nav" + navid){
+            const currentBtn = document.getElementById("nav"+ navid);
+            e.classList.add("disableColor");
+        }
+    })
+}
 
 displaySlides(currentslide);
+modifyNav(1);
+interval = setInterval(() => {
+    console.log(currentslide);
+    currentslide += 1;
+    displaySlides(currentslide);
+    modifyNav(currentslide);
+}, 1500);
+
